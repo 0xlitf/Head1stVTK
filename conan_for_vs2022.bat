4f17aaade23a5dfc8f86da930009019dd29a4082
0xlitf <0xlitf@gmail.com>
2023-09-15 10:25:14 +0800

    change bat

--- conan_for_vs2022.bat
+++ conan_for_vs2022.bat
@@ -1,11 +1,29 @@
+
+@echo off
+setlocal
+
+set "folderToCheck=./build_for_vs2022"
+
+rmdir /s /q "%folderToCheck%" 2>nul
+
+rem check
+if exist "%folderToCheck%" (
+    rem "%folderToCheck%" still exist, goto :eof
+    
+    pause
+    goto :eof
+) else (
+    echo "%folderToCheck%" not exist
+)
+
 conan profile detect --force
-conan install . --output-folder=./build_for_vs2022 --build=missing -s build_type=Debug
+conan install . --output-folder=./build_for_vs2022  --profile=default --build=missing -s build_type=Debug -s build_type=Release
 cmake --preset conan-default
 
 cd ./build_for_vs2022
 cmake .. -G "Visual Studio 17 2022" -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake
-cmake --build_for_vs2022 . --config Debug
+cmake --build . --config Debug --config Release
 
-
+pause
 
 
